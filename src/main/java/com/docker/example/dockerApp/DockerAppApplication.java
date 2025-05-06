222c7a887b56bbc6f36e39c97faddebe8a037cd3
Abhishek Patel <55614853+patelabhi787@users.noreply.github.com>
2025-05-05 19:03:00 -0500

    Add files via upload

---
+++ src/main/java/com/docker/example/dockerApp/DockerAppApplication.java
@@ -0,0 +1,20 @@
+package com.docker.example.dockerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerAppApplication.class, args);
+	}
+	
+	@GetMapping("/message")
+	public String helloWorld() {
+		return "Hello World";
+	}
+}
