ec72df9ca7dd1348d617f310e050eb941a527620
Abhishek Patel <patelabhi787@gmail.com>
2026-08-01 13:02:43 -0500

    add printMessage2

--- src/main/java/com/docker/example/dockerApp/Demo.java
+++ src/main/java/com/docker/example/dockerApp/Demo.java
@@ -5,5 +5,9 @@ public class Demo {
     public void printMessage() {
         System.out.println("Hello from Demo class!");
     }
+
+    public void printMessage2() {
+        System.out.println("Hello Divya from Demo class!");
+    }
     
 }
